11f613ad7cb0ce994cadbf2262785ae81e33b707
xiehq <xiehq@winhong.com>
2017-04-18 11:31:47 +0800

    chinese

--- Jenkinsfile@v2.groovy
+++ Jenkinsfile@v2.groovy
@@ -1,14 +1,9 @@
 #!groovy
-
 //timeout(time: 60, unit: 'SECONDS') {
 node {
-
-
         def mvnHome = tool "${mavenId}"
         env.JAVA_HOME = tool "${jdk}"
-
-        stage('‰ª£Á†Å‰∏ãËΩΩ') {
-
+        stage('¥˙¬Îœ¬‘ÿ') {
             try {
                 creid = "${SCMcredential}"
                 //  echo  'CREDENTIALS'
@@ -19,38 +14,26 @@ node {
             }
             //if (creid != null && creid != "") {
             echo ' credential id' + creid
-
             if (env.SCMTYPE == "git") {
-
                 scmtype = 'GitSCM'
-
                 echo 'using credential to git'
-
                 git credentialsId: creid, url: env.SCMUrl, branch: env.SCMBranch
-
             } else { //svn
                 echo 'using credential to svn'
                 scmtype = 'SubversionSCM'
-
                 checkout([$class: scmtype, additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[credentialsId: creid, depthOption: 'infinity', ignoreExternalsOption: true, local: '.', branches: [[name: '*/' + env.SCMBranch]],          \
                        remote: env.SCMUrl]], workspaceUpdater: [$class: 'UpdateUpdater']])
-
             }
 //+env.SCMBranch
-
-
         }
-
-        stage('ÁºñËØë') {
+        stage('±‡“Î') {
             CMD = "'${mvnHome}/bin/mvn' " + env.Compile_goal
             //-Dmaven.test.failure.ignore clean package"
             // CMD="'${mvnHome}/bin/mvn' -X " +env.Compile_goal
             echo CMD
-
             //sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean package"
             sh CMD
             //sh "'${mvnHome}/bin/mvn' -X '${Compile_goal}'"
-
         }
         echo env.Findbugs_skip
         if (env.Findbugs_skip == "false") {
@@ -73,57 +56,43 @@ node {
                     sh "exit 1"
                     return
                 }
-
             }
-
         }
         if (env.MavenTest_skip == "false") {
             //todo -Dfindbugs.includeFilterFile=./findbugsfilter.xml
-
             try {
-                stage("Maven ÊµãËØï") {
+                stage("Maven ≤‚ ‘") {
                     CMD = "'${mvnHome}/bin/mvn' " + env.MavenTest_goal
-
                     sh CMD
                 }
-
             }
             catch (exc) {
                 if (env.MavenTest_continueOnfail == "false") {
                                         throw exc
-
                     sh "exit 1"
                     return
                 } else {
                     status result: "FAILURE"
                 }
             }
-
         }
-
-
         if (env.OSWAPDepend_skip == "false") {
             //todo -Dfindbugs.includeFilterFile=./findbugsfilter.xml
-            stage("‰æùËµñÂåÖOSWAPÊ£ÄÊü•") {
+            stage("“¿¿µ∞¸OSWAPºÏ≤È") {
                 try {
                     sh "'${mvnHome}/bin/mvn'  org.owasp:dependency-check-maven:1.4.5:check -Ddependency-check-format=XML -DreportOutputDirectory=./target"
-
                 }
                 catch (exc) {
                     //TODO
                 }
-
             }
         }
-
         if (env.Artifact_skip == "false") {
             //todo -Dfindbugs.includeFilterFile=./findbugsfilter.xml
-
             try {
-                stage("ÊâìÂåÖÁªìÊûú") {
+                stage("¥Ú∞¸Ω·π˚") {
                     filename = env.BUILD_TAG + ".zip"
                     //echo filename
-
                     zip archive: true, dir: 'target', glob: '', zipFile: filename
                 }
             }
@@ -136,39 +105,28 @@ node {
                 }
                 //TODO
             }
-
-
         }
         if (env.Sonar_skip == "false") {
             //todo -Dfindbugs.includeFilterFile=./findbugsfilter.xml
-            //ËÆæÂÆö jdk version
-
-            //toDO Ëé∑Âèñsonar ‰ø°ÊÅØ
+            //…Ë∂® jdk version
+            //toDO ªÒ»°sonar –≈œ¢
             try {
                 // withCredentials([usernameColonPassword(credentialsId: 'svn-winhong', variable: '')]) {
                 // some block
                 //    sh 'echo uname=$USERNAME pwd=$PASSWORD'
                 //}
-
                 //node('<MY_SLAVE>') {
-
-                stage("ËøõË°åSonarÊ£ÄÊü•") {
+                stage("Ω¯––SonarºÏ≤È") {
                     withCredentials([[$class          : 'UsernamePasswordMultiBinding', credentialsId: env.SonarCredential,
                                       usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-
                         echo "credentialsId"
                         echo env.USERNAME
-
-
                         sh "'${mvnHome}/bin/mvn'  org.sonarsource.scanner.maven:sonar-maven-plugin:3.0.2:sonar \
         -Dsonar.host.url='${env.SonarUrl}' -Dsonar.login='${env.USERNAME}' -Dsonar.password='${env.PASSWORD}'"
                     }
                     //}
-
                     //sh 'echo uname=$USERNAME pwd=$PASSWORD'
-
                 }
-
             }
             catch (exc) {
                 if (env.Sonar_continueOnfail == "false") {
@@ -178,28 +136,18 @@ node {
                 } else {
                     status result: "FAILURE"
                 }
-
             }
-
-
         }
-
        
-//Docker Á≥ªÂàóÊìç‰Ωú
+//Docker œµ¡–≤Ÿ◊˜
         if (env.CreateImage_skip == "false") {
             def imagename=env.CreateImage_registry+'/'+env.CreateImage_tag+":"+env.BUILD_TAG 
             echo imagename
-            stage("ÂàõÂª∫ÈïúÂÉè") {
-
+            stage("¥¥Ω®æµœÒ") {
                 sh "docker build -t ${imagename} ."
-
-
             }
-
             if (env.PushImage_skip == "false") {
-
-                stage("PushÈïúÂÉè") {
-
+                stage("PushæµœÒ") {
 //'${imagename}'
                     sh "docker push   '${imagename}' "
                     sh " echo '${imagename}' >dockerbuildresult.txt"
@@ -208,31 +156,26 @@ node {
                     filename = java.net.URLEncoder.encode("image--"+imagename, "UTF-8")+".zip"
                     
                     archiveArtifacts filename
-
                     zip archive: true, dir:  dirname , glob: '', zipFile: filename
                     //sh "rm -rf  '${dirname}' "
                 }
-
             }
         //test data
         //imagename="10.0.2.50/library/busybox"
             if (env.DeployToRancher_skip == "false") {
-
-                stage("ÈÉ®ÁΩ≤Â∫îÁî®") {
+                stage("≤ø ”¶”√") {
                 try{
-// Â∑≤ÁªèÂ≠òÂú®ÁöÑÊúçÂä°ÈúÄË¶ÅÂÖàdown--todo
+// “—æ≠¥Ê‘⁄µƒ∑˛ŒÒ–Ë“™œ»down--todo
                     sh "rancher rm  '${DeployToRancher_service}' "
                 }
                 catch (exc) {
                 }
                     sh "rancher run ${DeployToRancher_arg} --name '${DeployToRancher_service}' '${imagename}' ${DeployToRancher_cmd} "
-
                 }
                 
             }
-
         }
         
         currentBuild.result = 'SUCCESS'
-
     }
+
