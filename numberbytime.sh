e110b8df09d80a35328d6417e0430bb779b86eee
radishgz <radishgz@gmail.com>
2017-04-19 13:44:57 +0800

    modify to adapted GUI requirements

--- numberbytime.sh
+++ numberbytime.sh
@@ -2,6 +2,7 @@
 #---- 统计脚本 
 #caution: java will read all output ,so be careful to add output content
 set -x 
+export LANG=C
 #the shell be call by java, so first make sure the work directory is corrent
 BASEDIR=$(dirname "$0")
 #echo "$BASEDIR"
@@ -32,20 +33,33 @@ lastDetail=$(mktemp ./tmp/jenkins-stat.XXXXXX)
 join -t $'\t' -j 1 allbuilds $runningFile  |awk ' {print $0" RUNNING"}' >$lastDetail
 join -t $'\t' -j 1 allbuilds allresults  >>$lastDetail
 
+
+lastDetail2=$(mktemp ./tmp/jenkins-stat.XXXXXX)
+
+cat $lastDetail|awk '{print $2" "$3}' >$lastDetail2
+#must sort before use awk time compare
+sort $lastDetail2 -o $lastDetail2
+
 #echo $lastDetail
 timeresult=$(mktemp ./tmp/jenkins-stat.XXXXXX)
-awk -v beginDate="$beginDate" -v endDate="$endDate"  -F'\t' '$2 >= beginDate { p=1 }  $2 >= endDate { p=0 }  p { print $0 }' $lastDetail >$timeresult
 
-result=$(mktemp ./tmp/jenkins-stat.XXXXXX)
+awk -v beginDate="$beginDate" -v endDate="$endDate"   '$1 >= beginDate { p=1 }  $1 >= endDate { p=0 }  p { print $0 }' $lastDetail2 >$timeresult
 
-
+tempresult=$(mktemp ./tmp/jenkins-stat.XXXXXX)
+ 
 
 #cat $timeresult |awk 'NR>0{arr[$4]++}END{print "[";for (a in arr) print "{statu: \""a"\",\"value\": \""arr[a]"\"},";print "]"}' >$result
-cat $timeresult |awk 'NR>0{ arr[$2]++ } END { for (a in arr) print a" "arr[a]" " }' >$result
+cat $timeresult |awk 'NR>0{ arr[substr($1,1,7)]++ } END { for (a in arr) print a" "arr[a]" " }' >$tempresult
 
 #cat temp112 |awk 'NR>0{arr[$4]++}END{print "[";for (i = $4-1; i >= 0; --i)  print "{statu=\""$4"\",\"value\"=\""arr[$4]"\"},";print "]"}' >$result
 
-cat $result
+#awk 'BEGIN {print "["} { if (NR >1)  { print "{\"runTime\":" p1",\"number\":"p2"}," } {p1=$1;p2=$2 } }  END { print "{\"runTime\":" p1",\"number\":"p2"}]" }' $result
+#cat  $tempresult
+tempresult2=$(mktemp ./tmp/jenkins-stat.XXXXXX)
+
+sort $tempresult >$tempresult2
+awk 'BEGIN {print "["} { if (NR >1)  { print "{\"runTime\":" p1"000000,\"number\":"p2"}," } {p1=$1;p2=$2 } }  END { print "{\"runTime\":" p1"000000,\"number\":"p2"}]" }' $tempresult2 
+#cat $result
 exit 8   
   
   
